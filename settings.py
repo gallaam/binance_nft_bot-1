93fa0d2d30f63456e94aefc7092ae0c0b80d0468
matvey <matveytri777@gmail.com>
2021-09-17 03:17:50 +0300

    clear cookie

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-CSRFTOKEN = 'fe51eb82cfd43a25c535bb970398b9c3'
-COOKIE = 'cid=lxWoF9wA; nft-init-compliance=true; _ga=GA1.2.685878306.1631781633; _gid=GA1.2.1062010891.1631781633; bnc-uuid=c1f84f25-55ba-4c71-84ca-3a83942c5e4d; source=referral; campaign=www.binance.com; userPreferredCurrency=RUB_USD; fiat-prefer-currency=RUB; home-ui-ab=A; s9r1=49D5D41F6570D435F7A85E409364AE78; lang=ru; cr00=95D7F99FB7B1BD5D1E7B96F635E51A99; d1og=web.134290911.FE338B44862A1977AB7A3A2E1A74E2DE; r2o1=web.134290911.4F549A3918AD1827A12578BDA92BFA40; f30l=web.134290911.E37CA8BED05D318501A344241782581D; logined=y; isAccountsLoggedIn=y; __BINANCE_USER_DEVICE_ID__={"c0fc45c195ba67179472510ca0147419":{"date":1631789374517,"value":"1631789374262VFvSQC8mOQByNufXu3o"}}; p20t=web.134290911.6F93F0FFFB68F7BBC4B1EC9DD412E028; sensorsdata2015jssdkcross={"distinct_id":"134290911","first_id":"17bedc3bebe7e9-09c2f3937aedf9-172a1d0f-2073600-17bedc3bebff85","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://www.google.com/"},"$device_id":"17bedc3bebe7e9-09c2f3937aedf9-172a1d0f-2073600-17bedc3bebff85"}; BNC_FV_KEY=31ffbb78c756aa7dbbb7c5e2341b68ad765885ff; BNC_FV_KEY_EXPIRE=1631910750117'
+CSRFTOKEN = ''
+COOKIE = ''
 
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)',
