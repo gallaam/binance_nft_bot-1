da600b41b181192aa3ca80c45b248e768c98ae98
matvey <matveytri777@gmail.com>
2021-09-18 00:11:36 +0300

    check headers

--- handlers.py
+++ handlers.py
@@ -1,2 +1,19 @@
+import requests
+
+from settings import headers
+from user.urls import user_info
+
+
 def event_is_not_over(status: int) -> bool:
     return status == 0
+
+def headers_is_right() -> bool:
+    response = requests.post(user_info, headers=headers)
+
+    if response.status_code == 200:
+        print('Successfully connected')
+        return True
+    else:
+        print('Something wrong...')
+        print('Check please: COOKIE, CSRFTOKEN, headers')
+        return False
