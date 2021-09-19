6cac481d3654363bc561a5605dd5901eff48aaa5
matvey <matveytri777@gmail.com>
2021-09-20 01:03:31 +0300

    fix cookie

--- settings.py
+++ settings.py
@@ -2,9 +2,10 @@ CSRFTOKEN = ''
 COOKIE = ''
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
     'clienttype': 'web',
-    'cookie': COOKIE.encode('utf-8'),
+    'cookie': COOKIE,
     'csrftoken': CSRFTOKEN,
-    'content-type': 'application/json'
+    'content-type': 'application/json',
+    'bnc-uuid': '53ab0ad4-06b1-4a2d-8b69-eefd495fc704'
 }
