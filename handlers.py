97a51866b4da60f846151e91097c478ee0eb0baa
matvey <matveytri777@gmail.com>
2021-09-22 21:41:09 +0300

    upd timedelta

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 
@@ -26,12 +26,11 @@ def send_requests_to_buy(box, start_sale_time: datetime):
     threads = list()
     while True:
         current_time = datetime.today()
-        if start_sale_time <= current_time:
+        if start_sale_time <= (current_time - timedelta(seconds=0.1)):
             for _ in range(1, 10000):
                 request = threading.Thread(target=box._buy_box)
                 request.start()
                 threads.append(request)
-                time.sleep(0.1)
 
             for thread in threads:
                 thread.join()
