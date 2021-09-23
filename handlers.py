90bb33f03e1918ed1bd23f577f1c302911be5c60
matvey <matveytri777@gmail.com>
2021-09-23 23:29:51 +0300

    +0.01sec

--- handlers.py
+++ handlers.py
@@ -5,13 +5,13 @@ from datetime import datetime, timedelta
 import requests
 
 from settings import headers
-from user.urls import user_info
 
 
 def event_is_not_over(status: int) -> bool:
     return status == 0
 
 def headers_is_right() -> bool:
+    user_info = 'https://www.binance.com/bapi/accounts/v1/private/account/user/base-detail'
     response = requests.post(user_info, headers=headers)
 
     if response.status_code == 200:
@@ -27,11 +27,11 @@ def send_requests_to_buy(box, start_sale_time: datetime):
     while True:
         current_time = datetime.today()
         if start_sale_time <= (current_time - timedelta(seconds=0.1)):
-            for _ in range(1, 10000):
+            for _ in range(1, 100):
                 request = threading.Thread(target=box._buy_box)
                 request.start()
                 threads.append(request)
-                time.sleep(0.05)
+                time.sleep(0.06)
 
             for thread in threads:
                 thread.join()
