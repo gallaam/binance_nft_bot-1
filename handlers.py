a62e8ba667acf49018c93d3462e7e5e22c794dc1
matvey <matveytri777@gmail.com>
2021-09-24 14:05:18 +0300

    update time

--- handlers.py
+++ handlers.py
@@ -22,12 +22,14 @@ def headers_is_right() -> bool:
         print('Check please: COOKIE, CSRFTOKEN, headers')
         return False
 
+# ToDo 2: experiment with time.sleep(x)
+# ToDo 1: random proxies
 def send_requests_to_buy(box, start_sale_time: datetime):
     threads = list()
     while True:
         current_time = datetime.today()
         if start_sale_time <= (current_time - timedelta(seconds=0.1)):
-            for _ in range(1, 100):
+            for _ in range(1, 1000):
                 request = threading.Thread(target=box._buy_box)
                 request.start()
                 threads.append(request)
