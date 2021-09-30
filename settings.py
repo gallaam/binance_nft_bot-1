0dae7a07aa7cef1dd521c43798809baf91d61630
Matvey Semenov <matveytri777@gmail.com>
2021-10-01 00:29:23 +0300

    upd settings

--- settings.py
+++ settings.py
@@ -1,12 +1,21 @@
+# Give it here - https://2captcha.com/enterpage
+CAPTCHA_API_KEY = ''
+
+BINANCE_SITEKEY = '6LeUPckbAAAAAIX0YxfqgiXvD3EOXSeuq0OpO8u_'
+
 CSRFTOKEN = ''
 COOKIE = ''
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-    'clienttype': 'web',
-    'cookie': COOKIE.encode('UTF-8'),
-    'csrftoken': CSRFTOKEN,
-    'content-type': 'application/json',
-    'bnc-uuid': '',
-    'device-info' : '',
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+    "bnc-uuid": "dc36fccf-9f08-4dc1-a13a-bd062d29f72e",
+    "clienttype": "web",
+    "content-type": "application/json",
+    "cookie": COOKIE.encode("UTF-8"),
+    "csrftoken": CSRFTOKEN,
+    "device-info": "",
+    "user-agent": "",
+    "lang": "en",
+    "x-nft-checkbot-sitekey": BINANCE_SITEKEY,
 }
