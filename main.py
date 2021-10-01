ec37331689c975c4b0b51256425e4ce87eb0565a
Matvey Semenov <matveytri777@gmail.com>
2021-10-01 21:49:01 +0300

    add info about box

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ avalible_boxes = box_info.get_avalible_boxes()
 box_info.log_info_boxes(avalible_boxes)
 
 selected_box = str(input('\nNumber of box: '))
-amount_boxes = int(input('\nAmount boxes: '))
+amount_boxes = int(input(f'\nAmount boxes (Max: {avalible_boxes[selected_box]["limit_amount"]}): '))
 
 product_id = avalible_boxes[selected_box]['product_id']
 box = Box(product_id=product_id, amount=amount_boxes)
 start_sale_time = box._get_start_sale_time
-
 
 print('Waiting for start')
 send_requests_to_buy(box, start_sale_time)
