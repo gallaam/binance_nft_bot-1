5ca3c1959768d4b487a9efeb39534a20ee34192f
Matvey Semenov <matveytri777@gmail.com>
2021-10-03 16:43:37 +0300

    add proxy

--- settings.py
+++ settings.py
@@ -3,6 +3,9 @@ CAPTCHA_API_KEY = ''
 
 BINANCE_SITEKEY = '6LeUPckbAAAAAIX0YxfqgiXvD3EOXSeuq0OpO8u_'
 
+# Yours own proxy here `username:password@ip:port` [NOT REQUIRE]
+PROXY = ''
+
 CSRFTOKEN = ''
 COOKIE = ''
 
@@ -14,8 +17,8 @@ headers = {
     "content-type": "application/json",
     "cookie": COOKIE.encode("UTF-8"),
     "csrftoken": CSRFTOKEN,
-    "device-info": "",
-    "user-agent": "",
+    "device-info": "eyJzY3JlZW5fcmVzb2x1dGlvbiI6IjE5MjAsMTA4MCIsImF2YWlsYWJsZV9zY3JlZW5fcmVzb2x1dGlvbiI6IjE5MjAsMTA4MCIsInN5c3RlbV92ZXJzaW9uIjoiTGludXggeDg2XzY0IiwiYnJhbmRfbW9kZWwiOiJ1bmtub3duIiwic3lzdGVtX2xhbmciOiJlbi1VUyIsInRpbWV6b25lIjoiR01UKzMiLCJ0aW1lem9uZU9mZnNldCI6LTE4MCwidXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk0LjAuNDYwNi42MSBTYWZhcmkvNTM3LjM2IiwibGlzdF9wbHVnaW4iOiJQREYgVmlld2VyLENocm9tZSBQREYgVmlld2VyLENocm9taXVtIFBERiBWaWV3ZXIsTWljcm9zb2Z0IEVkZ2UgUERGIFZpZXdlcixXZWJLaXQgYnVpbHQtaW4gUERGIiwiY2FudmFzX2NvZGUiOiJkNDRlNDk3YiIsIndlYmdsX3ZlbmRvciI6Ikdvb2dsZSBJbmMuIChub3V2ZWF1KSIsIndlYmdsX3JlbmRlcmVyIjoiQU5HTEUgKG5vdXZlYXUsIE5WMTI2LCBPcGVuR0wgNC4zIChDb3JlIFByb2ZpbGUpIE1lc2EgMjEuMC4zKSIsImF1ZGlvIjoiMTI0LjA0MzQ3NTI3NTE2MDc0IiwicGxhdGZvcm0iOiJMaW51eCB4ODZfNjQiLCJ3ZWJfdGltZXpvbmUiOiJFdXJvcGUvTW9zY293IiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgVjk0LjAuNDYwNi42MSAoTGludXgpIiwiZmluZ2VycHJpbnQiOiI2MGNmNDMxMmJhMjBkZjg0N2FjYzc5YjkxMjJmODQ4ZSIsImRldmljZV9pZCI6IiIsInJlbGF0ZWRfZGV2aWNlX2lkcyI6IjE2MzMyMTI3MjIyMTRTUnVhUE5YRGRrR2IxdG5HTWpWIn0=",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
     "lang": "en",
     "x-nft-checkbot-sitekey": BINANCE_SITEKEY,
 }
