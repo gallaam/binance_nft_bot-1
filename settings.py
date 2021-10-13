04cc8f824e0504937f8f68c46098ae73ece43171
Matvey Semenov <matveytri777@gmail.com>
2021-10-13 20:57:10 +0300

    add count requests

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+COUNT_REQUESTS = 15
+
 # Give it here - https://2captcha.com/enterpage
 CAPTCHA_API_KEY = ''
 
@@ -12,12 +14,12 @@ COOKIE = ''
 headers = {
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,ru;q=0.8",
-    "bnc-uuid": "dc36fccf-9f08-4dc1-a13a-bd062d29f72e",
+    "bnc-uuid": "",
     "clienttype": "web",
     "content-type": "application/json",
     "cookie": COOKIE.encode("UTF-8"),
     "csrftoken": CSRFTOKEN,
-    "device-info": "eyJzY3JlZW5fcmVzb2x1dGlvbiI6IjE5MjAsMTA4MCIsImF2YWlsYWJsZV9zY3JlZW5fcmVzb2x1dGlvbiI6IjE5MjAsMTA4MCIsInN5c3RlbV92ZXJzaW9uIjoiTGludXggeDg2XzY0IiwiYnJhbmRfbW9kZWwiOiJ1bmtub3duIiwic3lzdGVtX2xhbmciOiJlbi1VUyIsInRpbWV6b25lIjoiR01UKzMiLCJ0aW1lem9uZU9mZnNldCI6LTE4MCwidXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzk0LjAuNDYwNi42MSBTYWZhcmkvNTM3LjM2IiwibGlzdF9wbHVnaW4iOiJQREYgVmlld2VyLENocm9tZSBQREYgVmlld2VyLENocm9taXVtIFBERiBWaWV3ZXIsTWljcm9zb2Z0IEVkZ2UgUERGIFZpZXdlcixXZWJLaXQgYnVpbHQtaW4gUERGIiwiY2FudmFzX2NvZGUiOiJkNDRlNDk3YiIsIndlYmdsX3ZlbmRvciI6Ikdvb2dsZSBJbmMuIChub3V2ZWF1KSIsIndlYmdsX3JlbmRlcmVyIjoiQU5HTEUgKG5vdXZlYXUsIE5WMTI2LCBPcGVuR0wgNC4zIChDb3JlIFByb2ZpbGUpIE1lc2EgMjEuMC4zKSIsImF1ZGlvIjoiMTI0LjA0MzQ3NTI3NTE2MDc0IiwicGxhdGZvcm0iOiJMaW51eCB4ODZfNjQiLCJ3ZWJfdGltZXpvbmUiOiJFdXJvcGUvTW9zY293IiwiZGV2aWNlX25hbWUiOiJDaHJvbWUgVjk0LjAuNDYwNi42MSAoTGludXgpIiwiZmluZ2VycHJpbnQiOiI2MGNmNDMxMmJhMjBkZjg0N2FjYzc5YjkxMjJmODQ4ZSIsImRldmljZV9pZCI6IiIsInJlbGF0ZWRfZGV2aWNlX2lkcyI6IjE2MzMyMTI3MjIyMTRTUnVhUE5YRGRrR2IxdG5HTWpWIn0=",
+    "device-info": "",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
     "lang": "en",
     "x-nft-checkbot-sitekey": BINANCE_SITEKEY,
